16c46c4a910034cb065cd35dc9ea101ec762b66b
Andrey Mekeda <andreymekeda@gmail.com>
2023-01-03 20:57:27 +0200

    add new product

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from 'src/app/services/modal.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-create-product',
@@ -7,6 +9,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-product.component.scss'],
 })
 export class CreateProductComponent implements OnInit {
+  constructor(
+    public productService: ProductService,
+    public modalService: ModalService
+  ) {}
   form = new FormGroup({
     title: new FormControl<string>('', [
       Validators.required,
@@ -17,8 +23,21 @@ export class CreateProductComponent implements OnInit {
     return this.form.controls.title as FormControl;
   }
   submit() {
-    console.log(this.form.value);
+    this.productService
+      .create({
+        title: this.form.value.title as string,
+        price: 13.5,
+        description: 'Lorem ipsum set',
+        image: 'https://i.pravatar.cc',
+        category: 'electronic',
+        rating: {
+          rate: 42,
+          count: 1,
+        },
+      })
+      .subscribe(() => {
+        this.modalService.close();
+      });
   }
-  constructor() {}
   ngOnInit(): void {}
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,7 +5,7 @@ import {
   HttpParams,
 } from '@angular/common/http';
 import { IProduct } from '../models/product';
-import { catchError, delay, Observable, retry, throwError } from 'rxjs';
+import { catchError, delay, Observable, retry, tap, throwError } from 'rxjs';
 import { ErrorService } from './error.service';
 
 @Injectable({
@@ -13,12 +13,23 @@ import { ErrorService } from './error.service';
 })
 export class ProductService {
   constructor(private http: HttpClient, private errorService: ErrorService) {}
+  products: IProduct[] = [];
   getAll(): Observable<IProduct[]> {
     return this.http
       .get<IProduct[]>('https://fakestoreapi.com/products', {
         params: new HttpParams().append('limit', 5),
       })
-      .pipe(delay(2000), retry(2), catchError(this.errorHandler.bind(this)));
+      .pipe(
+        delay(2000),
+        retry(2),
+        tap((products) => (this.products = products)),
+        catchError(this.errorHandler.bind(this))
+      );
+  }
+  create(product: IProduct): Observable<IProduct> {
+    return this.http
+      .post<IProduct>('https://fakestoreapi.com/products', product)
+      .pipe(tap((prod) => this.products.push(prod)));
   }
   private errorHandler(error: HttpErrorResponse) {
     this.errorService.handle(error.message);
